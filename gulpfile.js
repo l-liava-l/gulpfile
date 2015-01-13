f86ca47a383c9f2333cf99fd43506b286da79ef8
l-liava-l <legkodymov.lev@gmail.com>
2015-01-13 13:26:28 +0300

    v0.03

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 
 var dependens = ['fs', 'path', 'gulp', 'gulp-csso', 'gulp-uglify',
                 'gulp-concat', 'gulp-less', 'gulp-bower',
-                'gulp-watch', 'main-bower-files', 'karma', 'child_process'];
+                'gulp-watch', 'main-bower-files', 'karma', 
+                'child_process', 'gulp-html-tag-include'];
 
 dependens.forEach(function(name) {
     global[name.replace('gulp-', '')] = require(name);
@@ -12,11 +13,10 @@ dependens.forEach(function(name) {
 (function() {
 
     var config = {
-        pathToBuildDir: '/home/lev/projects/solvy/gulped-solvy/_public/',
-        pathToDevDir: '/home/lev/projects/solvy/gulped-solvy/front-end/',
-        minification: true,
-        watching: true,
-        testing: false
+        pathToBuildDir: '/home/lev/projects/solvy/v2.1/_public/',
+        pathToDevDir: '/home/lev/projects/solvy/v2.1/front-end/',
+        minification: false,
+        watching: true
     };
 
 
@@ -45,22 +45,23 @@ dependens.forEach(function(name) {
         gulp.task(moduleName + ':assets', assets);
         gulp.task(moduleName + ':vendor', vendor);
         gulp.task(moduleName + ':karma', testsKarma);
-        gulp.task(moduleName + ':installDep', installDep);
+        gulp.task(moduleName + ':installDep', installDep);        
+        gulp.task(moduleName + ':htmlInclude', htmlInclude);
 
- 
-
-
-        gulp.task(moduleName, function() {
+       
+        gulp.task(moduleName, function() {   
+            gulp.run(moduleName + ':vendor');   
             gulp.run(moduleName + ':js');
+            gulp.run(moduleName + ':htmlInclude');
             gulp.run(moduleName + ':assets');
             gulp.run(moduleName + ':styles');
-            gulp.run(moduleName + ':vendor');
 
             if(config.testing) {
                 gulp.run(moduleName + ':karma');
             }
         });
 
+        gulp.task(moduleName + ':lib')
 
         function js() {
             var src = gSrc + '/js/**/*.js',
@@ -75,12 +76,12 @@ dependens.forEach(function(name) {
         }
 
         function styles() {
-            var src =  gSrc + '/styles/**/*',
+            var src =  gSrc + '/includes.less',
                 dest = gDest + "/";
 
             gulp.src(src)
                 .pipe(less({
-                    paths: [ path.join(__dirname, 'less', 'includes') ]
+                    paths: [ path.join() ]
                 }))
                 .pipe(concat(moduleName + '.css'))
                 .pipe(min ? csso() : gulp.dest(dest))
@@ -99,6 +100,17 @@ dependens.forEach(function(name) {
             if(watch) gulp.watch(src, function() {  gulp.run(moduleName + ':assets'); });
         }
 
+        function htmlInclude() {
+            var src =  gSrc + '/templates/**/*.html',
+                dest = gDest + "/";
+
+            gulp.src(src)
+                .pipe(global['html-tag-include']())
+                .pipe(gulp.dest(dest));
+
+            if(watch) gulp.watch(src, function() {  gulp.run(moduleName + ':htmlInclude'); });
+        }
+
        function vendor() {
 
             var jsStatic = gSrc + '/lib/**/*.js',
@@ -112,8 +124,14 @@ dependens.forEach(function(name) {
             srcJs.push(jsStatic);
             srcCss.push(cssStatic);
 
-            gulp.src(srcJs).pipe(concat(moduleName + '.vendor.js')).pipe(gulp.dest(cssJsDest));
-            gulp.src(srcCss).pipe(concat(moduleName + '.vendor.css')).pipe(gulp.dest(cssJsDest));
+            gulp.src(srcJs)
+                .pipe(concat(moduleName + '.vendor.js'))
+                .pipe(min ? uglify() : gulp.dest(cssJsDest))
+                .pipe(gulp.dest(cssJsDest));
+            gulp.src(srcCss)
+                .pipe(concat(moduleName + '.vendor.css'))
+                .pipe(min ? csso() : gulp.dest(cssJsDest))
+                .pipe(gulp.dest(cssJsDest));
             gulp.src(mainBowerFiles(/\.eot$|\.svg$|\.ttf$|\.woff$/)).pipe(gulp.dest(fonts));
 
             function mainBowerFiles(filter) {
@@ -151,18 +169,26 @@ dependens.forEach(function(name) {
             createModuleTasks(name, false);
         })
 
-        gulp.task('all', function() {
+        gulp.task('build', function() {
             modules.forEach(run)
             function run(name) {gulp.run(name)}
         });
 
+        gulp.task('install', function() {
+            modules.forEach(run)
+            function run(name) {
+                gulp.run(name + ':installDep'); 
+            }
+        });
+
         gulp.task('default', function(){
             console.log('=============================================================');
-            console.log('  gulp [moduleName]:[type] -- build files in module by type.');
-            console.log('  gulp [moduleName] -- build module.');
-            console.log('  gulp all -- build all modules.');
+            console.log('  gulp install  -- Will be installed all dependens for each module.')
+            console.log('  gulp build  -- Build all.')
+            console.log('  gulp [moduleName] -- Build a module.');
+            console.log('  gulp [moduleName]:[type] -- Build files by type.');
             console.log('                        -------                          ');
-            console.log('  types: js, styles, assets, vendor, installDep');
+            console.log('  types: js, styles, assets, vendor, installDep, htmlInclude');
             console.log('  modules: ' + modules.join(', '));
             console.log('                        -------                          ');
             console.log('  gulp editor:karma -- run tests and native karma watcher');
